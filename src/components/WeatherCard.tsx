de852b599ae1176e9ed1d8692923acee5e21a8b8
szudev <sz.ubiergo@gmail.com>
2022-12-20 21:08:48 -0300

    border added to weathercard component

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -7,7 +7,7 @@ type WeatherCardProps = {
 
 const WeatherCard = ({ location, current }: WeatherCardProps) => {
   return (
-    <div className="m-auto flex max-w-5xl flex-col">
+    <div className="m-auto flex max-w-5xl flex-col rounded-2xl border border-youtube-light-border-color dark:border-youtube-dark-border-color">
       <span className="dark:text-white">{location.country}</span>
       <span className="dark:text-white">{location.name}</span>
       <span className="dark:text-white">{location.region}</span>
