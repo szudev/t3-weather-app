74503190ff42523741c1445964c84cad8fac04f6
szudev <sz.ubiergo@gmail.com>
2022-12-20 21:04:34 -0300

    little changes added

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -7,8 +7,10 @@ type WeatherCardProps = {
 
 const WeatherCard = ({ location, current }: WeatherCardProps) => {
   return (
-    <div className="m-auto flex max-w-5xl">
-      <span className="text-white">{location.country}</span>
+    <div className="m-auto flex max-w-5xl flex-col">
+      <span className="dark:text-white">{location.country}</span>
+      <span className="dark:text-white">{location.name}</span>
+      <span className="dark:text-white">{location.region}</span>
     </div>
   );
 };
