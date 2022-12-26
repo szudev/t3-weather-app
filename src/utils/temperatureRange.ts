d8591cc196f611041de3456f654c7f9c974bcadd
szudev <sz.ubiergo@gmail.com>
2022-12-25 22:23:14 -0300

    changed temperaturerange function's undefined checking

--- src/utils/temperatureRange.ts
+++ src/utils/temperatureRange.ts
@@ -1,5 +1,5 @@
 const temperatureRangeColors = (temperature: number | undefined) => {
-  if (!temperature) return "text-black dark:text-white";
+  if (typeof temperature === "undefined") return "text-black dark:text-white";
   if (temperature <= 13) return "text-blue-600";
   if (temperature > 13 && temperature <= 21) return "text-lime-400";
   if (temperature > 21 && temperature <= 29) return "text-amber-400";
