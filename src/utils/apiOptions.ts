af4740763f4dd71690e493d07914244a071b3c6f
Sebastián Zárate Ubiergo <sz.ubiergo@gmail.com>
2023-05-17 14:07:34 -0400

    Update apiOptions.ts
    Updated

--- src/utils/apiOptions.ts
+++ src/utils/apiOptions.ts
@@ -1,7 +1,7 @@
 export const apiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "13dca3e567mshf4b1e700d29b50bp120dfdjsn617b8571acf9",
-    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.X-RapidAPI-Key,
+    "X-RapidAPI-Host": process.env.X-RapidAPI-Host,
   },
 };
